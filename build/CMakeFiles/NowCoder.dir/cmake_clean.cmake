115010ce37379d43b7c3199307cd90a21749bef0
kbjcx <leitch.lu@outlook.com>
2023-02-22 23:55:59 +0800

    first commit

---
+++ build/CMakeFiles/NowCoder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NowCoder.dir/main.cpp.o"
+  "NowCoder"
+  "NowCoder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NowCoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
